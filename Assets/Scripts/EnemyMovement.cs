b2770ff6236079bdad25bf9288d4562592e993ee
qqwwee771441 <104188251+qqwwee771441@users.noreply.github.com>
2025-03-30 22:18:03 +0900

    feat: hw1
    hw1 done

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private GameObject target;
+    private float speed = 1.0f;
+
+    public void SetTarget(GameObject target)
+    {
+        this.target = target;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = target.transform.position - transform.position;
+        direction = direction / direction.magnitude;
+        transform.position += direction * speed * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/EnemyRespawn.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class EnemyRespawn : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject enemyPrefab;
+    private float time = 2f;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time >= 3f)
+        {
+            RespawnEnemy();
+            time = 0f;
+        }
+    }
+    private void RespawnEnemy()
+    {
+        GameObject enemy = Instantiate(enemyPrefab);
+        enemy.transform.position = player.transform.position + new Vector3(Random.Range(5f, -5f), Random.Range(5f, -5f));
+        enemy.GetComponent<EnemyMovement>().SetTarget(player);
+    }
+}

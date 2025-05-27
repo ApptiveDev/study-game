f27e9b8477e7db1fd6c0e3f29400bbe24fb48f74
hong <anjhu@pusan.ac.kr>
2025-05-27 19:59:46 +0900

    feat(poolManager.cs)
    6주차 실습

--- Assets/Hong/scripts/manager/PoolManager.cs
+++ Assets/Hong/scripts/manager/PoolManager.cs
@@ -3,31 +3,67 @@ using System.Collections.Generic;
 namespace AJH{
     public class PoolManager : MonoBehaviour
     {
-        public GameObject[] prefabs;
 
-        List<GameObject>[] pools;    
-    
+        public static PoolManager Instance;
+        int poolCount = 20;
+        [SerializeField] private GameObject poolObject;
+        [SerializeField] List<GameObject> poolObjectList = new List<GameObject>();
 
-        public GameObject Get(int index) 
+
+        private void Awake()
         {
-            GameObject select = null;
-
-            foreach (GameObject item in pools[index])
+            if (Instance == null)
             {
-                if (!item.activeSelf)
-                {
-                    select = item;
-                    select.SetActive(true);
-                    break;
-                }
+                Instance = this;
+            }
+            else
+            {
+                Destroy(gameObject);
             }
 
-            if (!select) {
-                select = Instantiate(prefabs[index], transform);
-                pools[index].Add(select);
+            for (int i = 0; i < poolCount; i++)
+            {
+                GameObject pooledObject = Instantiate(poolObject, transform);
+                pooledObject.SetActive(false);
+                poolObjectList.Add(pooledObject);
             }
-            return select;
         }
+
+
+        public GameObject GetObject()
+        {
+            GameObject pooledObject = poolObjectList[0];
+            pooledObject.SetActive(true);
+            pooledObject.transform.SetParent(null);
+            poolObjectList.RemoveAt(0);
+            return pooledObject;
+
+        }
+        // public GameObject[] prefabs;
+
+        // List<GameObject>[] pools;    
+
+
+        // public GameObject Get(int index) 
+        // {
+        //     GameObject select = null;
+
+        //     foreach (GameObject item in pools[index])
+        //     {
+        //         if (!item.activeSelf)
+        //         {
+        //             select = item;
+        //             select.SetActive(true);
+        //             break;
+        //         }
+        //     }
+
+        //     if (!select) {
+        //         select = Instantiate(prefabs[index], transform);
+        //         pools[index].Add(select);
+        //     }
+        //     return select;
+        // }
 
     }
 }
